1ca9004a97b60ec98df63a1a642f7d3b39bdd83f
markrose <rosenlofm@gmail.com>
2018-04-25 18:46:03 -0400

    First alterations to enemy ship for levels

---
+++ Assets/Scripts/Controller Scripts/EnemyShipLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShipLevel : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
